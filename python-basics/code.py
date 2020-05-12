9a2b03e7709440617193934e1a83ac099fef4991
yashwanth-ds <yashwanth-ds@users.noreply.github.com>
2020-05-12 10:20:05 +0530

    Code file created.

---
+++ python-basics/code.py
@@ -0,0 +1,70 @@
+# --------------
+# Code starts here
+class_1=['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+# Create the lists 
+class_2=['Hilary Mason','Carla Gentry','Corinna Cortes']
+# Concatenate both the strings
+
+new_class=class_1+class_2
+print(new_class)
+new_class.remove("Carla Gentry")
+print(new_class)
+
+# Append the list
+
+# Print updated list
+
+
+# Remove the element from the list
+
+# Print the list
+# Create the Dictionary
+courses={"Math":65,"English":70,"History":80,"French":70,"Science":60}
+total=sum(courses.values())
+percentage=total*100/500
+print(percentage)
+mathematics={'Geoffrey Hinton':78,'Andrew Ng':95,'Sebastian Raschka':65,'Yoshua Benjio':50,'Hilary Mason':70,'Corinna Cortes':66,'Peter Warden':75}
+
+topper= max(mathematics,key=mathematics.get)
+print (topper)
+first_name=topper.split()[0]
+last_name=topper.split()[1]
+certificate_name=(last_name+" "+first_name).upper()
+print(certificate_name)
+
+
+# Slice the dict and stores  the all subjects marks in variable
+
+
+# Store the all the subject in one variable `Total`
+
+# Print the total
+
+# Insert percentage formula
+
+# Print the percentage
+
+
+
+
+# Create the Dictionary
+ 
+
+
+
+# Given string
+
+
+# Create variable first_name 
+
+# Create variable Last_name and store last two element in the list
+
+# Concatenate the string
+
+# print the full_name
+
+# print the name in upper case
+
+# Code ends here
+
+
